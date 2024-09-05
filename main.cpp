c3f41e774bbf9dfbbbaca5fa218da7f91f7897f2
Semison2 <semison2@ut.utm.edu>
2024-09-05 17:55:56 -0500

    Added product function and updated the main for the sum

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+int sum(int n);
+int product(int n);
+
+
+int main() {
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+
+  cout << "The sum from 1 to " << n << " is " << sum(n) << endl;
+  cout << "Product from 1 to " << n << " is " << product(n) << endl;
+  return 0;
+}
+
 int sum(int n) {
   int total = 0;
   for (int i = 1; i <= n; ++i) {
@@ -11,14 +25,10 @@ int sum(int n) {
   return total;
 }
 
-
-int main() {
-  int n;
-  cout << "Enter a number: ";
-  cin >> n;
-
-  int result = sum(n);
-  cout << "The sum from 1 to " << n << " is " << result << endl;
-  
-  return 0;
+int product(int n) {
+  int total = 1;
+  for (int i = 1; i <= n; ++i) {
+    total *= i;
+  }
+  return total;
 }
