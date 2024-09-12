2f76f0d8cc599f9af7f1c2c333f4f7953e1619d0
Semison2 <semison2@ut.utm.edu>
2024-09-12 14:45:23 -0500

    Basics added for Problem2+3

---
+++ HWO_0.5/Problem2+3/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @author Sam Emison
+ * @date 2024-09-12
+ * @brief Asks user for input for array and Reads file
+ * 
+ * Reads based on user input
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Please specify the size of the array: ";
+  int arraySize;
+  cin >> arraySize;
+  
+  return 0;
+}
