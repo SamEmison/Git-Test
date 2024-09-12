8dc6087b8a3dec167bbe35c7abe254566e8589c7
Semison2 <semison2@ut.utm.edu>
2024-09-12 11:15:09 -0500

    Added a main for problem 1/5 and added a integers file to read from

---
+++ HWO_0.5/Problem1+5/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @author Sam Emison
+ * @date 2024-09-12
+ * @brief Sentinal Loop with Averaging Capabilities
+ * 
+ * Sentinal loop takes in array and Averages the numbers and prints them out
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+//Constant Variable MAX_ARRAY
+const int MAX_ARRAY=1000;
+
+
+int main() {
+  static int arr[MAX_ARRAY];
+
+  
+  return 0;
+}
